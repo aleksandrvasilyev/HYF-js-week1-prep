d14d42ca47483ab061692c6a1de57c014b4bf6e7
Alexander Vasyliev <saddon11@gmail.com>
2024-10-20 19:00:02 +0200

    fix functions call

--- database2/createDb.js
+++ database2/createDb.js
@@ -1,11 +1,14 @@
 import fs from "fs/promises";
 import connection from "./connection.js";
 
-try {
-  const sql = await fs.readFile(`${import.meta.dirname}/db.sql`, "utf-8");
-  await connection.query(sql);
-} catch (error) {
-  console.error(error);
-} finally {
-  await connection.end();
-}
+const main = async () => {
+  try {
+    const sql = await fs.readFile(`${import.meta.dirname}/db.sql`, "utf-8");
+    await connection.query(sql);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await connection.end();
+  }
+};
+main();
--- database2/getData.js
+++ database2/getData.js
@@ -1,50 +1,53 @@
 import fs from "fs/promises";
 import connection from "./connection.js";
 
-try {
-  await connection.query(`USE recipes;`);
+const main = async () => {
+  try {
+    await connection.query(`USE recipes;`);
 
-  const getAllVegetarianRecipesWithSalt = await connection.query(`
-    SELECT 
-    r.name
-    FROM recipes r
-    JOIN recipes_categories rc 
+    const getAllVegetarianRecipesWithSalt = await connection.query(`
+      SELECT 
+      r.name
+      FROM recipes r
+      JOIN recipes_categories rc 
+        ON r.id = rc.recipe_id
+      JOIN categories c 
+        ON rc.category_id = c.id
+      JOIN recipes_ingredients ri 
+        ON ri.recipe_id = r.id
+      JOIN ingredients i 
+        ON i.id = ri.ingredient_id
+      WHERE c.name = 'Vegetarian' AND i.name = 'Salt';
+  `);
+    console.log(getAllVegetarianRecipesWithSalt[0]);
+
+    const getAllNoBakingCakesRecipes = await connection.query(`
+      SELECT r.name 
+      FROM recipes r
+      JOIN recipes_categories rc
       ON r.id = rc.recipe_id
-    JOIN categories c 
-      ON rc.category_id = c.id
-    JOIN recipes_ingredients ri 
-      ON ri.recipe_id = r.id
-    JOIN ingredients i 
-      ON i.id = ri.ingredient_id
-    WHERE c.name = 'Vegetarian' AND i.name = 'Salt';
-`);
-  console.log(getAllVegetarianRecipesWithSalt[0]);
+      JOIN categories c
+      ON c.id = rc.category_id
+      WHERE c.name in ('Cake', 'No-Bake')
+      GROUP BY r.name
+      HAVING COUNT(DISTINCT c.name) = 2;`);
+    console.log(getAllNoBakingCakesRecipes[0]);
 
-  const getAllNoBakingCakesRecipes = await connection.query(`
-    SELECT r.name 
-    FROM recipes r
-    JOIN recipes_categories rc
-    ON r.id = rc.recipe_id
-    JOIN categories c
-    ON c.id = rc.category_id
-    WHERE c.name in ('Cake', 'No-Bake')
-    GROUP BY r.name
-    HAVING COUNT(DISTINCT c.name) = 2;`);
-  console.log(getAllNoBakingCakesRecipes[0]);
+    const getAllVeganOrJapaneseRecipes = await connection.query(`
+       SELECT r.name 
+      FROM recipes r
+      JOIN recipes_categories rc
+      ON r.id = rc.recipe_id
+      JOIN categories c
+      ON c.id = rc.category_id
+      WHERE c.name = 'Vegan'
+      OR c.name = 'Japanese';`);
+    console.log(getAllVeganOrJapaneseRecipes[0]);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await connection.end();
+  }
+};
 
-  const getAllVeganOrJapaneseRecipes = await connection.query(`
-     SELECT r.name 
-    FROM recipes r
-    JOIN recipes_categories rc
-    ON r.id = rc.recipe_id
-    JOIN categories c
-    ON c.id = rc.category_id
-    WHERE c.name = 'Vegan'
-    OR c.name = 'Japanese';`);
-  console.log(getAllVeganOrJapaneseRecipes[0]);
-
-} catch (error) {
-  console.error(error);
-}
-
-connection.end();
+main();
